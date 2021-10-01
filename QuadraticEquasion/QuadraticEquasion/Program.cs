3688b9e338dc6b403b882ea6ea4b8e338349f80d
asponomarev <82184898+asponomarev@users.noreply.github.com>
2021-10-01 21:35:59 +0300

    main function done

---
+++ QuadraticEquasion/QuadraticEquasion/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace QuadraticEquasion
+{
+    /**
+        @brief Главный класс  
+
+        Данный класс содержит основную функцию.
+    */
+    class Program
+    {
+        /**
+        @brief Главная функция
+        
+        Точка входа в программу      
+        */
+        static void Main()
+        {
+            QuadEq();
+        }
+
+        /**
+        @brief Функция, организующая процесс вычисления корней квадратного уравнения.
+        
+        Данная функция в цикле осуществляет получение данных,
+        передачу полученных данных на проверку и
+        передачу корректных данных на вычисление.       
+        */
+        public static void QuadEq()
+        {
+            string args = Console.ReadLine();
+
+            while (args != "quit")
+            {
+                if (Helper.CheckArgs(args))
+                {
+                    Helper.FindRoots(args);
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect arguments");
+                }
+
+                Console.WriteLine();
+                args = Console.ReadLine();
+            }
+        }
+
+    }
+
+}
